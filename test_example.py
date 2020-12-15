c0d3719fa7e18f52a3ee4711310b94b048b379a0
Irina Nechaeva <ivn@tut.by>
2020-12-15 20:25:56 +0300

    Test example

---
+++ test_example.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    driver.get("http://www.google.com/")
+    driver.quit()
