f724dc99ac21cc14e8337a6f921554c909d709b8
Irina Nechaeva <ivn@tut.by>
2021-01-06 15:25:14 +0300

    Task 3

--- test_example.py
+++ test_example.py
@@ -1,10 +1,13 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Firefox()
+    wd = webdriver.Chrome()
     print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
@@ -12,4 +15,5 @@ def driver(request):
 
 def test_example(driver):
     driver.get("http://www.google.com/")
-    driver.quit()
+    driver.find_element_by_name('q').send_keys('webdriver', Keys.ENTER)
+    WebDriverWait(driver,10).until(EC.title_is('webdriver - Поиск в Google'))
